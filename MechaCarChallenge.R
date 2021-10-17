6aa4a7753a8adcd8ad6a2a1161818e6fabc033dd
Qian8988 <86527347+Qian8988@users.noreply.github.com>
2021-10-16 21:12:10 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,26 +1,26 @@
-# Deliverable 1 --------------------
+# Deliverable 1  
 library(dplyr)
 
 # Import and read CSV file as a dataframe
-MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
-#perform linear regression
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
+# Linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar_mpg)
 
-#perform summary data to determine p-value and r-squared value
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
+# Summary data to determine p-value and r-squared value
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar_mpg))
 
-#  Deliverable 2------------------------------------------------------------
+#  Deliverable 2 
 # Import and read CSV file  
 Suspension <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
 
 # Get a total summary dataframe on PSI column
-summarize_demo <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep') 
+total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep') 
 
 # Get a lot summary dataframe
-lot_demo <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
 
-#  Deliverable 3 -----------------------------------------------------------
+#  Deliverable 3  
 
 # Peform t-test across all Lots
 t.test(Suspension$PSI,mu = 1500)
